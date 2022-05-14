482fda0a1fe47cf3a01b60f478d30dc0611e2650
oksanaozcan <mekansa83@gmail.com>
2022-05-14 23:59:01 +0300

    refactoring CulturesImport with ToModel

--- app/Http/Controllers/Admin/Culture/ImportController.php
+++ app/Http/Controllers/Admin/Culture/ImportController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Admin\Culture;
 
 use App\Http\Controllers\Controller;
 use App\Imports\CulturesImport;
+use App\Models\ImportStatus;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
 use Throwable;
 
@@ -20,10 +22,25 @@ class ImportController extends Controller
     {
       $request->validate([
         'file' => 'required',
+      ]);     
+
+      $file = $request->file('file')->store('import');   
+      $importStatus = ImportStatus::create([
+        'path' => $file,
+        'importable' => 'App\Models\Culture',
+        'status' => 'processing',
+        'user_id' => auth()->id()
       ]);
-      $file = $request->file('file')->store('import');
-      $import = new CulturesImport;
-      $import->import($file);
-      return back()->withStatus('Import done!');         
+      try {
+        Excel::import(new CulturesImport, request()->file('file'));
+        //if success -> 
+        // ImportStatus::find($importStatus->id)->update([
+        //   'status' => 'success',
+        //   'user_id' => auth()->id()
+        // ]);
+        return back()->withStatus('Import done!');
+      } catch (Throwable $e) {
+        report($e);
+      }                
     }
 }
--- app/Imports/CulturesImport.php
+++ app/Imports/CulturesImport.php
@@ -4,43 +4,35 @@ namespace App\Imports;
 
 use App\Models\Culture;
 use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToModel;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\Importable;
-use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Throwable;
 
-class CulturesImport implements ToCollection, WithHeadingRow, WithChunkReading, ShouldQueue
+class CulturesImport implements ToModel, WithHeadingRow, WithChunkReading, ShouldQueue
 {
   use Importable;
-    /**
-    * @param Collection $collection
-    */
-    public function collection(Collection $collection)
+    
+    public function model(array $row)
     {
-      foreach ($collection as $row) 
-      {
-        if ($row->filter()->isNotEmpty())
-        {          
-          Culture::firstOrCreate([
-          'name' => $row['name']
-          ], [
-            'name' => $row['name'],
-            'nitrogen' => $row['nitrogen'],
-            'phosphorus' => $row['phosphorus'],
-            'potassium' => $row['potassium'],
-            'fertilizer_id' => $row['fertilizer_id'],           
-            'region' => $row['region'],
-            'price' => $row['price'],
-            'description' => $row['description'],
-            'purpose' => $row['purpose']
-          ]);
-        }
-      }
+      return new Culture([
+        'name' => $row['name'],
+        'nitrogen' => $row['nitrogen'],
+        'phosphorus' => $row['phosphorus'],
+        'potassium' => $row['potassium'],
+        'fertilizer_id' => $row['fertilizer_id'],           
+        'region' => $row['region'],
+        'price' => $row['price'],
+        'description' => $row['description'],
+        'purpose' => $row['purpose']
+      ]);        
     }
 
     public function chunkSize(): int
     {
-        return 5000;
+        return 1000;
     }
 }
