2791c8d649f20777ce9fe51a06b500d7b10e3335
oksanaozcan <mekansa83@gmail.com>
2022-06-02 21:54:23 +0300

    refact

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,12 +4,14 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
  
   protected function schedule(Schedule $schedule)
   {
+    Log::info("cron started");
     $schedule->command('queue:work')->everyMinute()->withoutOverlapping();
   }
 
